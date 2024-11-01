d1ca25ba95082c40bf06c35f866f0b07a4811ed2
delprafede <delpradofederico0@gmail.com>
2024-11-01 17:34:29 -0300

    agregando productos al carrito

--- componentes/Guitarras.jsx
+++ componentes/Guitarras.jsx
@@ -1,57 +1,26 @@
-import { useState } from "react";
-import { db } from "../dataBase/db";
-
-export const Guitarras = () => {
-  const [data, setData] = useState(db);
-  const [cart, setCart] = useState([db]);
-
-  const addToCart = (elem) => {
-    const itemExists = cart.findIndex((guitar) => guitar.id === elem.id);
-    if (itemExists >= 0) {
-    //   alert("El producto ya está en el carrito");
-      const updatecart = [...cart]
-      updatecart[itemExists].quantity++;
-      setCart(updatecart)
-    } else {
-      elem.quantity = 1;
-      alert("El producto ha sido agregado al carrito");
-      setCart([...cart, elem]);
-    }
-
-    // setCart([...cart, elem]);
-  };
-
-  return data.map((elem) => {
-    return (
-      <div
-        key={elem.id}
-        className="col-md-6 col-lg-4 my-4 row align-items-center"
-      >
-        <div className="col-4">
-          <img
-            className="img-fluid"
-            src={`/img/${elem.image}.jpg`}
-            alt="hola"
-          />
-        </div>
-        <div className="col-8">
-          <h3 className="text-black fs-4 fw-bold text-uppercase">
-            {elem.name}
-          </h3>
-          <p>{elem.description}</p>
-          <p className="fw-black text-primary fs-3">${elem.price}</p>
-
-          <button
-            type="button"
-            className="btn btn-dark w-100"
-            onClick={() => addToCart(elem)}
-          >
-            Agregar al Carrito
-          </button>
-        </div>
+export const Guitarras = ({ guitar, setCart, cart }) => {
+  const { id, name, description, image, price } = guitar;
+ 
+  return (
+    <div className="col-md-6 col-lg-4 my-4 row align-items-center">
+      <div className="col-4">
+        <img className="img-fluid" src={`/img/${image}.jpg`} alt="hola" />
       </div>
-    );
-  });
+      <div className="col-8">
+        <h3 className="text-black fs-4 fw-bold text-uppercase">{name}</h3>
+        <p>{description}</p>
+        <p className="fw-black text-primary fs-3">${price}</p>
+
+        <button
+          type="button"
+          className="btn btn-dark w-100"
+          onClick={() => setCart((previewCart) => [...previewCart, guitar])}
+        >
+          Agregar al Carrito
+        </button>
+      </div>
+    </div>
+  );
 };
 
 {
